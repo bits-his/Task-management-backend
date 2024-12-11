5bf0a568bdfa99f13fffa55203d249544af501b3
ahmadkabir <atomakabir001@gmail.com>
2024-12-11 19:12:42 +0100

    save

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,10 @@ export default (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
+      user_id: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
       fullname: DataTypes.STRING,
       email: {
         type: DataTypes.STRING,
@@ -14,6 +18,7 @@ export default (sequelize, DataTypes) => {
       address: DataTypes.STRING,
       password: DataTypes.STRING,
       role: DataTypes.STRING,
+      status: DataTypes.STRING,
       startups: DataTypes.STRING,
       starting_date: DataTypes.STRING,
       end_date: DataTypes.STRING,
