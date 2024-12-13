a77071355155f8bcecb9adbd591031dbff4ad482
abdulsalam8 <abdulsalamabubakar2023@gmail.com>
2024-12-13 10:29:35 +0100

    save

--- src/controllers/excuse.js
+++ src/controllers/excuse.js
@@ -4,7 +4,7 @@ import db from "../models";
 
 export const postExcuse = (req,res)=>{
 console.log(req.body);
-    const {create_by="",excuse_type="",excuse_day="",status="",excuse_description="",approved_by="",id=0}=req.body;
+    const {create_by="",excuse_type="",excuse_day="2024-12-12",status="",excuse_description="",approved_by="",id=0}=req.body;
     const{query_type=""}=req.query;
 
     db.sequelize.query(`CALL excuses(:create_by,:excuse_type,:excuse_day,:excuse_description,:status,:approved_by,:query_type)`,{
