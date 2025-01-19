64e283595cace6192a8d363eac2dd9eb8a885512
abdulsalam8 <abdulsalamabubakar2023@gmail.com>
2025-01-19 12:57:26 +0100

    save

--- src/controllers/stats.js
+++ src/controllers/stats.js
@@ -21,3 +21,90 @@ console.log(req.body);
         console.log(err);
       });
 }
+
+export const getAllStartupMembers = (req, res) => {
+  db.sequelize.query(`CALL GetStartupStats()`, {
+    type: db.Sequelize.QueryTypes.SELECT
+  })
+  .then((response) => {
+    const [totalStats, taskStatusDistribution, taskStatusByStartup, taskProgressTimeline, weeklyTaskComparison, milestoneProgress, issueResolution, attendanceTrends, startupAttendance, highAbsenteeism] = response;
+    
+
+    const startupStats = {
+
+      total_startups: totalStats[0].total_startups,
+      total_completed_tasks: totalStats[0].total_completed_tasks,
+      attendance_rate: totalStats[0].attendance_rate,
+      weekly_reports_submitted: totalStats[0].weekly_reports_submitted,
+      milestones_achieved: totalStats[0].milestones_achieved,
+
+ 
+      task_status_distribution: taskStatusDistribution.map(item => ({
+        status: item.status,
+        count: item.count
+      })),
+      task_status_by_startup: taskStatusByStartup.map(item => ({
+        startup: item.startup,
+        pending: item.pending,
+        inProgress: item.in_progress,
+        completed: item.completed
+      })),
+      task_progress_timeline: taskProgressTimeline.map(item => ({
+        date: item.date,
+        [item.startup]: item.task_count
+      })),
+      performance_metrics: milestoneProgress.map(item => ({
+        startup: item.startup,
+        completion_time: 85, 
+        total_tasks: 45, 
+        high_priority: item.total,
+        overdue: 2 
+      })),
+
+
+      weekly_task_comparison: weeklyTaskComparison.map(item => ({
+        startup: item.startup,
+        planned: item.planned,
+        completed: item.completed
+      })),
+      milestone_progress: milestoneProgress.map(item => ({
+        startup: item.startup,
+        achieved: item.achieved,
+        total: item.total
+      })),
+      issue_resolution: issueResolution.map(item => ({
+        startup: item.startup,
+        resolved: item.resolved,
+        unresolved: item.unresolved
+      })),
+
+   
+      overall_attendance: 85,
+      attendance_trends: attendanceTrends.map(item => ({
+        date: item.date,
+        rate: item.rate
+      })),
+      startup_attendance: startupAttendance.map(item => ({
+        startup: item.startup_name,
+        rate: item.attendance_rate
+      })),
+      high_absenteeism: highAbsenteeism.map(item => ({
+        startup: item.startup_name,
+        absent_days: item.absent_days
+      })),
+
+
+      startups_data: [], 
+      users_by_role: [
+        { name: "Senior Developers", value: 15 },
+        { name: "Interns", value: 8 },
+        { name: "SIWES", value: 12 }
+      ]
+    };
+    
+    res.json({ success: true, data: startupStats });
+  })
+  .catch((err) => {
+    res.json({ success: false, message: err.message });
+  });
+};
