0f3f97bf513750e418de767d47646df73581fc76
aiialpha <aii07038713563@gmail.com>
2025-02-04 16:28:32 +0100

    save

--- src/routes/attendance.routes.js
+++ src/routes/attendance.routes.js
@@ -5,4 +5,12 @@ module.exports = (app) => {
   app.post('/api/attendance/signout', signOut);
   app.get('/api/attendance/status', getTodayStatus);
   app.get('/api/attendance/history', getAttendanceHistory);
+  app.get("/check-router", async (req, res) => {
+    try {
+      const response = await fetch("http://192.168.1.1");
+      res.json({ connected: true });
+    } catch (error) {
+      res.json({ connected: false });
+    }
+  });
 };
