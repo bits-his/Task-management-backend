d4e1e1c951b56198dd4b739987a3913c34b4a4a1
ahmadkabir <atomakabir001@gmail.com>
2025-03-02 17:58:16 +0100

    save

--- src/controllers/pushController.js
+++ src/controllers/pushController.js
@@ -2,28 +2,35 @@
 const webpush = require("web-push");
 const db = require("../models");
 
-
 // Set your VAPID keys here
-const publicVapidKey = process.env.VAPID_PUBLIC_KEY|| "YOUR_PUBLIC_VAPID_KEY";
-const privateVapidKey = process.env.VAPID_PRIVATE_KEY || "YOUR_PRIVATE_VAPID_KEY";
+const publicVapidKey = process.env.VAPID_PUBLIC_KEY || "YOUR_PUBLIC_VAPID_KEY";
+const privateVapidKey =
+  process.env.VAPID_PRIVATE_KEY || "YOUR_PRIVATE_VAPID_KEY";
 
-webpush.setVapidDetails(
-  "mailto:aii07038713563@gmail.com",
-  publicVapidKey,
-  privateVapidKey
-);
+// webpush.setVapidDetails(
+//   "mailto:aii07038713563@gmail.com",
+//   publicVapidKey,
+//   privateVapidKey
+// );
+
+// webPush.setVapidDetails(
+//   "mailto:your-email@example.com", // Replace with your email
+//   vapidKeys.publicKey,
+//   vapidKeys.privateKey
+// );
 
 // Save subscription to MySQL
 const saveSubscription = (subscription) => {
   return new Promise((resolve, reject) => {
     const { endpoint, keys } = subscription;
 
-    db.sequelize.query(
-      `INSERT INTO subscriptions (endpoint, data_keys,user_id) VALUES (?, ?,?)`,
-      {
-        replacements: [endpoint, JSON.stringify(keys),1],
-      }
-    )
+    db.sequelize
+      .query(
+        `INSERT INTO subscriptions (endpoint, data_keys,user_id) VALUES (?, ?,?)`,
+        {
+          replacements: [endpoint, JSON.stringify(keys), 1],
+        }
+      )
       .then((result) => {
         resolve(result);
       })
@@ -38,16 +45,15 @@ const subscribeUser = (req, res) => {
   const subscription = req.body;
 
   saveSubscription(subscription)
-    .then(() =>{
-     return res.status(201).json({success: true, message: "Subscription saved successfully" })
-    }
-    )
-    .catch((err) =>{
-
-      console.log(err)
-     return res.status(500).json({ error: "Error saving subscription" })
-    }
-    );
+    .then(() => {
+      return res
+        .status(201)
+        .json({ success: true, message: "Subscription saved successfully" });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ error: "Error saving subscription" });
+    });
 };
 
 // Send notification to all subscribers
@@ -56,31 +62,32 @@ const sendNotificationToAll = (req, res) => {
 
   const payload = JSON.stringify({ title: "New Notification", body: message });
 
-  db.sequelize.query("SELECT * FROM subscriptions")
+  db.sequelize
+    .query("SELECT * FROM subscriptions")
     .then((results) => {
-
-      const subscriptions = results[0].map(subscription => ({endpoint: subscription.endpoint, keys: JSON.parse(subscription.data_keys)}));
+      const subscriptions = results[0].map((subscription) => ({
+        endpoint: subscription.endpoint,
+        keys: JSON.parse(subscription.data_keys),
+      }));
       Promise.all(
         subscriptions.map((subscription) => {
-
           return webpush.sendNotification(subscription, payload);
         })
       )
-        .then(() =>{
-console.log("Notification sent successfully")
-          res.status(200).json({ message: "Notification sent successfully" })
-        }
-        )
-        .catch((error) =>
-          console.error("Error sending notifications:", error) ||
-          res.status(500).json({ error: "Error sending notifications" })
+        .then(() => {
+          console.log("Notification sent successfully");
+          res.status(200).json({ message: "Notification sent successfully" });
+        })
+        .catch(
+          (error) =>
+            console.error("Error sending notifications:", error) ||
+            res.status(500).json({ error: "Error sending notifications" })
         );
     })
     .catch((err) => {
       res.status(500).json({ error: "Error fetching subscriptions" });
     });
 };
-
 
 // Send push notification
 const sendPushNotification = async (subscription, payload) => {
@@ -103,6 +110,5 @@ const sendPushNotification = async (subscription, payload) => {
 //   // Example: db.query('DELETE FROM subscriptions WHERE endpoint = ?', [endpoint]);
 //   console.log(`Removed subscription with endpoint: ${endpoint}`);
 // };
-
 
 module.exports = { subscribeUser, sendNotificationToAll };
