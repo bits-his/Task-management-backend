eddb6736e6cd36c5e4002e7a5aa4602e5d756d2c
uniboika <nazifabdullahi003@gmail.com>
2025-03-12 18:18:14 +0100

    save

--- src/routes/task_form.js
+++ src/routes/task_form.js
@@ -1,8 +1,16 @@
-import { get_task_form, task_form, update_task_status, updateSubTask } from "../controllers/task_form";
-import { upload } from '../config/multerConfig';
+import {
+  get_task_form,
+  task_form,
+  update_task_status,
+  updateAssignee,
+  updateSubTask,
+} from "../controllers/task_form";
+import { upload } from "../config/multerConfig";
 module.exports = (app) => {
-  app.post("/api/create_task", upload.array('images', 5), task_form);
+  app.post("/api/create_task", upload.array("images", 5), task_form);
   app.post("/api/update_task_status", update_task_status);
   app.get("/api/get_task", get_task_form);
   app.post("/api/update_sub_task", updateSubTask);
+  app.get("/api/update_sub_task", updateSubTask);
+  app.post("/api/update_assignee", updateAssignee);
 };
